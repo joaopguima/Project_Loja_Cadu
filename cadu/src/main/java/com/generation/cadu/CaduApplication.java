ba91d369ba574916336bd2ff02ca05986166738c
Weida Winy <winnyweida@hotmail.com>
2024-07-05 13:47:30 -0300

    Criação do projeto spring
    Feat:
    - Projeto inicializado pelo spring initializer
    
    Docs:
    - Organização das pastas do projeto
    - Alteração do nome do repositório

---
+++ cadu/src/main/java/com/generation/cadu/CaduApplication.java
@@ -0,0 +1,13 @@
+package com.generation.cadu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CaduApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CaduApplication.class, args);
+	}
+
+}
---
+++ cadu/src/test/java/com/generation/cadu/CaduApplicationTests.java
@@ -0,0 +1,13 @@
+package com.generation.cadu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CaduApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
