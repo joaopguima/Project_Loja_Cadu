eecd0f45e5f54b71cb67492d59d3b29d5fcc0a79
Weida Winy <winnyweida@hotmail.com>
2024-07-23 12:21:09 -0300

    Aleracao para o deploy
    - Adicionei o email de um dos integrantes

--- src/main/java/com/generation/cadu/configuration/SwaggerConfig.java
+++ src/main/java/com/generation/cadu/configuration/SwaggerConfig.java
@@ -6,7 +6,6 @@ import org.springframework.context.annotation.Configuration;
 
 import io.swagger.v3.oas.models.ExternalDocumentation;
 import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
 import io.swagger.v3.oas.models.responses.ApiResponse;
